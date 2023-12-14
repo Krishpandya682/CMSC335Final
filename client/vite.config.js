e76867216aa25407294597a57e8ddab02f8b9fb8
Krish Pandya <kpandya@umd.edu>
2023-12-14 02:31:02 -0500

    All hosts allowed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// vite.config.js
+import allowedHostsPlugin from "vite-plugin-allowed-hosts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    allowedHostsPlugin({
+      hosts: "all",
+    }),
+  ],
+});
